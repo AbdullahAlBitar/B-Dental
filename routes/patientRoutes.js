7cee49b38b0ec91bb8bcb792746461560a04b08d
AbdullahAlBitar <abdullahalbitar2002@gmail.com>
2024-10-29 14:17:59 +0300

    get patient prf

--- routes/patientRoutes.js
+++ routes/patientRoutes.js
@@ -7,7 +7,7 @@ router.get('/', patientController.getPatients);
 router.get('/:id', patientController.getPatientById);
 // router.post('/:id', patientController.);
 router.patch('/:id', patientController.updatePatient);
-router.post('/profile/:id', patientController.getPatientProfile);
+router.get('/profile/:id', patientController.getPatientProfile);
 // router.get('/:id/edit', patientController.getEditPatientForm);
 // router.post('/', patientController.createPatient);
 // router.delete('/:id', patientController.deletePatient);
