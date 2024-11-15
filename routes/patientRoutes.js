99b97dc0338714b26bf4ba189fac6829aa76ecfb
AbdullahAlBitar <abdullahalbitar2002@gmail.com>
2024-11-15 14:10:34 +0300

    post for patient create without id

--- routes/patientRoutes.js
+++ routes/patientRoutes.js
@@ -6,8 +6,8 @@ const { patientCreate, patientUpdate } = require('./validationSchemas');
 const router = express.Router();
 
 router.get('/', patientController.getPatients);
+router.post('/',validate(patientCreate), patientController.createPatient);
 router.get('/:id', patientController.getPatientById);
-router.post('/:id',validate(patientCreate), patientController.createPatient);
 router.patch('/:id',validate(patientUpdate), patientController.updatePatient);
 router.get('/profile/:id', patientController.getPatientProfile);
 // router.get('/:id/edit', patientController.getEditPatientForm);
